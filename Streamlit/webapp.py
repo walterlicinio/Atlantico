790c1bb3812ced870bcb3303f31d14cb7639e180
Walter Licinio <walter.licinio@gmail.com>
2022-05-09 02:12:20 -0300

    streamlit

---
+++ Streamlit/webapp.py
@@ -0,0 +1,52 @@
+# A streamlit data app that receives a pdf input and outputs the tokenized and lemmatized text
+import string
+import streamlit as st
+import nltk as nltk
+import pdfplumber
+import matplotlib.pyplot as plt
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('word_tokenize')
+
+print("oi")
+st.title("Streamlit Data App")
+st.header("Tokenize and Lemmatize")
+
+pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+if pdf:
+    text = ''
+    with pdfplumber.open(pdf) as pdf_file:
+        for page in pdf_file.pages:
+            text += page.extract_text()
+    text = text.replace('\n', ' ')
+    # remove all punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    st.header("Original Text")
+    st.write('First 1000 chars:')
+    st.write(text[:1000])    
+    
+    text_tokenized = nltk.word_tokenize(text)
+    st.header('Tokenized text')  
+    st.write(text_tokenized)
+   
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    text_lemmatized = [lemmatizer.lemmatize(w) for w in text_tokenized]
+    st.header('Lemmatized text')  
+    st.write(text_lemmatized)
+    
+    # remove all stopwords portuguese
+    stopwords = nltk.corpus.stopwords.words('portuguese')
+    text_lemmatized = [w for w in text_lemmatized if w not in stopwords]
+    
+    st.header('Plotting the frequency of the lemmatized text')
+    freq = nltk.FreqDist(text_lemmatized)
+    freq.plot(20)
+    plt.show()
+    st.pyplot()
+ 
