572f604c4868d2a8605c85db8564b4697bf0d55c
Walter Licinio <walter.licinio@gmail.com>
2022-05-10 19:57:34 -0300

    Localização pt-br

--- Streamlit/webapp.py
+++ Streamlit/webapp.py
@@ -1,13 +1,13 @@
 # A streamlit data app that receives a pdf input and outputs the tokenized and lemmatized text
+
+# Imports & NLTK Downloads
 import string
 import streamlit as st
 import nltk 
 import pdfplumber
 import matplotlib.pyplot as plt
 import regex as re
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
+import pandas as pd
 
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -15,49 +15,61 @@ nltk.download('stopwords')
 nltk.download('word_tokenize')
 nltk.download('omw-1.4')
 
-print("oi")
-st.title("Streamlit Data App")
-st.header("Tokenize and Lemmatize")
-
-pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
+# Streamlit Global
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Atlantico - Data App - Tokenizer")
+st.header("Tokenizar e Lematizar")
+st.write("Envie um arquivo PDF para ser tokenizado, lemmatizado e analisado.")
+pdf = st.file_uploader("Upload de arquivo pdf.", type=["pdf"])
 
 if pdf:
     text = ''
     with pdfplumber.open(pdf) as pdf_file:
         for page in pdf_file.pages:
             text += page.extract_text()
-    text = text.replace('\n', ' ')
-    # remove all punctuation using re sub
+    text = text.replace('\n', ' ')   
+    # Remove all punctuation using re sub
     text = re.sub(r'[^\w\s]', '', text)
-    # text = text.translate(str.maketrans('', '', string.punctuation))
-    # remove all digits using re sub
+    # Remove all digits using re sub
     text = re.sub(r'\d+', '', text)
-    # text = text.translate(str.maketrans('', '', string.digits))
-    st.header("Original Text")
-    st.write('First 1000 chars:')
+    st.header("Texto original")
+    st.write('**Primeiros mil caracteres abaixo**:')
     st.write(text[:1000])   
     
     # decapitalize all words
     text = text.lower() 
         
     text_tokenized = nltk.word_tokenize(text)
-    st.header('Tokenized text')  
+    st.header('Texto Tokenizado')  
     st.write(text_tokenized)
    
     lemmatizer = nltk.stem.WordNetLemmatizer()
     text_lemmatized = [lemmatizer.lemmatize(w) for w in text_tokenized]
  
     
-    # remove all stopwords portuguese
+    # Remove all stopwords in portuguese
     stopwords = nltk.corpus.stopwords.words('portuguese')
-    # st.write(stopwords)
     text_lemmatized = [w for w in text_lemmatized if w not in stopwords]
-    st.header('Lemmatized text')  
+    st.header('Texto Lematizado')  
     st.write(text_lemmatized)
     
-    st.header('Plotting the frequency of the lemmatized text')
+    # Term Frequency
+    st.header('Frequência de Termos')
+    st.write('### Gráfico de frequência de termos')
     freq = nltk.FreqDist(text_lemmatized)
     freq.plot(20)
     plt.show()
     st.pyplot()
+
+    df = pd.DataFrame(freq.items(), columns=['term', 'freq'])
+
+    # Generate a table with the column being the word and the row being the document frequency
+    st.write("### Tabela de frequência de termos")
+    st.dataframe(freq.most_common())
+    st.pyplot()
+    
+    
+
+    
+    
  
