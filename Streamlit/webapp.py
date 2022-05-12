781c6c1938c366e34cb5c6036aa530a52a4796d6
Walter Licinio <walter.licinio@gmail.com>
2022-05-11 21:42:19 -0300

    DF & IDF

--- Streamlit/webapp.py
+++ Streamlit/webapp.py
@@ -2,6 +2,7 @@
 
 # Imports & NLTK Downloads
 import string
+import math
 import streamlit as st
 import nltk 
 import pdfplumber
@@ -17,57 +18,75 @@ nltk.download('omw-1.4')
 
 # Streamlit Global
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title("Atlantico - Data App - Tokenizer")
-st.header("Tokenizar e Lematizar")
-st.write("Envie um arquivo PDF para ser tokenizado, lemmatizado e analisado.")
-pdf = st.file_uploader("Upload de arquivo pdf.", type=["pdf"])
+st.title("Análise de Texto - NLP")
+# st.header("Desenvolvido por Walter Licínio")
+st.write("Envie um ou mais arquivos PDF para serem tokenizados, lemmatizados e analisados.")
+pdf_list = st.file_uploader("Upload de arquivo pdf.", type=["pdf"], accept_multiple_files=True)
+st.write("Para resetar a aplicação, remova os arquivos.")
 
-if pdf:
-    text = ''
-    with pdfplumber.open(pdf) as pdf_file:
-        for page in pdf_file.pages:
-            text += page.extract_text()
-    text = text.replace('\n', ' ')   
-    # Remove all punctuation using re sub
-    text = re.sub(r'[^\w\s]', '', text)
-    # Remove all digits using re sub
-    text = re.sub(r'\d+', '', text)
-    st.header("Texto original")
-    st.write('**Primeiros mil caracteres abaixo**:')
-    st.write(text[:1000])   
-    
-    # decapitalize all words
-    text = text.lower() 
+
+all_lemmatized_docs=[]    
+
+def tokenizer(pdf_list):
+    if pdf_list:   
+        for pdf  in pdf_list:
+            text = ''
+            with pdfplumber.open(pdf) as pdf_file:
+                for page in pdf_file.pages:
+                    text += page.extract_text()
+            text = text.replace('\n', ' ')   
+            text = re.sub(r'[^\w\s]', '', text)
+            text = re.sub(r'\d+', '', text)
+            st.header("Arquivo: "+pdf.name)
+            st.write('**Primeiros 200 caracteres abaixo**:')
+            st.write(text[:100] + "...")   
         
-    text_tokenized = nltk.word_tokenize(text)
-    st.header('Texto Tokenizado')  
-    st.write(text_tokenized)
-   
-    lemmatizer = nltk.stem.WordNetLemmatizer()
-    text_lemmatized = [lemmatizer.lemmatize(w) for w in text_tokenized]
- 
+            text = text.lower()         
+            text_tokenized = nltk.word_tokenize(text)
+        
+            lemmatizer = nltk.stem.WordNetLemmatizer()
+            text_lemmatized = [lemmatizer.lemmatize(w) for w in text_tokenized]
+        
+            stopwords = nltk.corpus.stopwords.words('portuguese')
+            text_lemmatized = [w for w in text_lemmatized if w not in stopwords]
     
-    # Remove all stopwords in portuguese
-    stopwords = nltk.corpus.stopwords.words('portuguese')
-    text_lemmatized = [w for w in text_lemmatized if w not in stopwords]
-    st.header('Texto Lematizado')  
-    st.write(text_lemmatized)
-    
-    # Term Frequency
-    st.header('Frequência de Termos')
-    st.write('### Gráfico de frequência de termos')
-    freq = nltk.FreqDist(text_lemmatized)
-    freq.plot(20)
-    plt.show()
-    st.pyplot()
-
+            
+            for word in text_lemmatized:
+                all_lemmatized_docs.append(word)
+            
+            # Term Frequency
+            st.header('TF - Frequência de Termos - '+ pdf.name )
+            # st.write('### Gráfico de frequência de termos - '+ pdf.name)
+            freq = nltk.FreqDist(text_lemmatized)
+            # freq.plot(30)
+            # plt.show()
+            # st.pyplot()
+            st.dataframe(freq.most_common())
+            st.markdown("""---""")
   
-    # Generate a table with the column being the word and the row being the document frequency
-    st.write("### Tabela de frequência de termos")
-    st.dataframe(freq.most_common(), width=800)
-    st.pyplot()
+
+def doc_frequency(pdf_list):  
+    if pdf_list:
+        st.write("# DF - Document Frequency")
+        st.write("Ocorrência de cada termo no conjunto de documentos.")
+        freq = nltk.FreqDist(all_lemmatized_docs)
+        st.dataframe(freq.most_common())
+        st.markdown("""---""")
+def inverse_document_frequency(pdf_list):
+    if pdf_list:
+        st.write("# IDF - Inverse Document Frequency")
+        st.write("Onde o valor de IDF é o logaritmo da divisão entre o número de documentos e o número de documentos em que o termo aparece.")
+        freq = nltk.FreqDist(all_lemmatized_docs)
+        idf = {}
+        for word in freq.keys():
+            idf[word] = math.log(len(pdf_list) / freq[word])
+        idf_df = pd.DataFrame.from_dict(idf, orient='index')
+        st.dataframe(idf_df)
+        st.markdown("""---""")
     
-    
+tokenizer(pdf_list)
+doc_frequency(pdf_list)
+inverse_document_frequency(pdf_list)
 
     
     
